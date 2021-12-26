203ac7782ebcccc9ab97cc894b1fc384eaf98741
Armando <armando@cornershopapp.com>
2021-12-25 21:44:51 -0500

    add suspend modifier to domain module functions

--- domain/src/main/java/pe/devpicon/android/cleanarch/domain/usecase/GetBooksUseCase.kt
+++ domain/src/main/java/pe/devpicon/android/cleanarch/domain/usecase/GetBooksUseCase.kt
@@ -1,12 +1,14 @@
 package pe.devpicon.android.cleanarch.domain.usecase
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import pe.devpicon.android.cleanarch.domain.model.Book
 import pe.devpicon.android.cleanarch.domain.repository.BookRepository
 
 class GetBooksUseCase(
     private val bookRepository: BookRepository
 ) {
-    operator fun invoke(): List<Book> {
-        return bookRepository.getBooks()
+    suspend operator fun invoke(): List<Book> = withContext(Dispatchers.Default) {
+        bookRepository.getBooks()
     }
 }
