692426319f79427b8b3ea19e136f54f7768b9b25
Armando <armando@cornershopapp.com>
2021-12-25 21:45:14 -0500

    connect viewmodel with usecase

--- presentation/src/main/java/pe/devpicon/android/mycleanarchitectureapplication/ui/listbook/ListBookViewModel.kt
+++ presentation/src/main/java/pe/devpicon/android/mycleanarchitectureapplication/ui/listbook/ListBookViewModel.kt
@@ -3,14 +3,22 @@ package pe.devpicon.android.mycleanarchitectureapplication.ui.listbook
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import pe.devpicon.android.cleanarch.domain.usecase.GetBooksUseCase
+import pe.devpicon.android.mycleanarchitectureapplication.ui.mapper.PresentationMapper
 import javax.inject.Inject
 
 @HiltViewModel
 class ListBookViewModel @Inject constructor(
-    private val getBooksUseCase: GetBooksUseCase
+    private val getBooksUseCase: GetBooksUseCase,
+    private val presentationMapper: PresentationMapper
 ) : ViewModel() {
+
+    init {
+        getBookViewList()
+    }
 
     private var _bookViewList: MutableLiveData<List<BookView>> = MutableLiveData(
         listOf<BookView>(
@@ -24,5 +32,12 @@ class ListBookViewModel @Inject constructor(
     )
     val bookViewList: LiveData<List<BookView>> = _bookViewList
 
+
+    private fun getBookViewList(){
+        viewModelScope.launch {
+            val bookViewList = getBooksUseCase().map { presentationMapper.mapFromDomain(it) }
+            _bookViewList.postValue(bookViewList)
+        }
+    }
 
 }
