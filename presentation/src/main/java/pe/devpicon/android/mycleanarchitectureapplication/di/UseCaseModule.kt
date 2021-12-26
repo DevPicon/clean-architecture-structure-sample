fd632c702078e434704aeb0523e0733beb6d2ee5
Armando <armando@cornershopapp.com>
2021-12-25 20:05:13 -0500

    add domain hilt-module

---
+++ presentation/src/main/java/pe/devpicon/android/mycleanarchitectureapplication/di/UseCaseModule.kt
@@ -0,0 +1,21 @@
+package pe.devpicon.android.mycleanarchitectureapplication.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import pe.devpicon.android.cleanarch.domain.repository.BookRepository
+import pe.devpicon.android.cleanarch.domain.usecase.GetBooksUseCase
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object UseCaseModule {
+
+    @Provides
+    fun provideGetBooksUseCase(
+        bookRepository: BookRepository
+    ): GetBooksUseCase {
+        return GetBooksUseCase(bookRepository)
+    }
+
+}
