0c420f8569cf86d5730439c6d623abe73c210c52
Armando <armando@cornershopapp.com>
2021-12-25 21:43:06 -0500

    remove unused dependencies

--- presentation/src/main/java/pe/devpicon/android/mycleanarchitectureapplication/ui/MainViewModel.kt
+++ presentation/src/main/java/pe/devpicon/android/mycleanarchitectureapplication/ui/MainViewModel.kt
@@ -1,10 +1,8 @@
 package pe.devpicon.android.mycleanarchitectureapplication.ui
 
 import androidx.lifecycle.ViewModel
-import pe.devpicon.android.cleanarch.domain.usecase.AddBookUseCase
 
 class MainViewModel(
-    val addBookUseCase: AddBookUseCase
 ) : ViewModel() {
 }
 
